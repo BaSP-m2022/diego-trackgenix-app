c3bd84d86f7d791891442225f6d6a5851a350739
Julián Cano <juliancano.m96@gmail.com>
2022-05-31 23:48:39 -0300

    BaSP-TG-35: WIP edit task from list

---
+++ src/Components/Tasks/EditTask.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+/* import React, { useState } from 'react'; */
+import styles from './form.module.css';
+
+const EditTask = (/* { editTask } */) => {
+  return (
+    <div>
+      <h2>Edit Task</h2>
+      <form className={styles.form}>
+	  </form>
+    </div>
+  );
+};
+
+export default EditTask;
